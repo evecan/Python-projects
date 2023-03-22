e416a81b05dfcb3590556c77bb885a5e95c8b09c
evecan <evec3064@gmail.com>
2023-03-22 12:33:45 +1100

    Initial Commit

--- week06/len_function.py
+++ week06/len_function.py
@@ -1,9 +1,9 @@
-countries = ["Australia", "USA", "UK", "Japan", "China", "Brazil"]
+countries = ["Australia", "USA", "UK", "Japan", "China", "Brazil", "Russia"]
 word = "word"
-people = {"john": "smith", "george": "anderson"}
+people = {"john": "smith", "george": "anderson", "micheal": "angelo"}
 print("number of countries", len(countries))  # the length(number) of items in the list
 print("number of letters", len(word))  # the number of characters in a word
 print("number of letters", len(people))  # the number of people in a dictionary
 
-print("the second country", countries[2])  # item in the list where count starts from 0
+print("the second country", countries[0])  # item in the list where count starts from 0
 print(people["john"])
